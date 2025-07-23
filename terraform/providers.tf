09af820af3f2a1823d7e1c01ce0d9954fe9bdcdf
Yadin Sarfati <yadin.sarfati@gmail.com>
2025-07-23 15:35:26 +0300

    Add files via upload

---
+++ terraform/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+  default_tags {
+    tags = {
+      Owner           = "Yadin-TF-task1"
+      expiration_date = "30-07-25"
+    }
+  }
+}
